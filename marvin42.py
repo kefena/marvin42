95dff8485fd6afa5b9ccc78f5c667b496fe14b14
kefena <kefenadiabe@gmail.com>
2019-05-13 10:56:21 +0000

    Initial commit

--- marvin42.py
+++ marvin42.py
@@ -1,23 +1,27 @@
 #!/usr/bin/env python3
 
-#version 8
+# importing all the liberies for this program
 from ev3dev2.motor import LargeMotor,OUTPUT_A, OUTPUT_B, SpeedPercent, MoveTank
 from ev3dev2.sensor.lego import InfraredSensor
 from ev3dev2.sound import Sound
 from ev3dev2.sensor.lego import ColorSensor
 import time
 
+# creating some variable
 move_A_and_B = MoveTank(OUTPUT_A, OUTPUT_B)
 mA = LargeMotor(OUTPUT_A)
 mB = LargeMotor(OUTPUT_B)
 
+# this function will  move the the tank front and  back, left and right
 def move_Tank(motor_A, motor_B):
 	while True:
 		move_A_and_B.on(motor_A, motor_B)
+
 try:
 	if __name__ == '__main__'
 		move_Tank(2, 3)
 
 except:
+	# this  will set all the the motors to 0
         move_A_and_B.reset()
 
